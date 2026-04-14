c9914505740ecaed61798a5eae42be373d8c5acf
Lauren <164786167+Laurennkr@users.noreply.github.com>
2026-04-14 09:10:01 -0500

    commit init

---
+++ src/test/java/desarrolloempresarial/com/quiz2romerocastrogarcia/Quiz2RomeroCastroGarciaApplicationTests.java
@@ -0,0 +1,13 @@
+package desarrolloempresarial.com.quiz2romerocastrogarcia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz2RomeroCastroGarciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
