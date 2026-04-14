0be4565c58ed2f580e03552cfd639a5cf38f9e0c
Lauren <164786167+Laurennkr@users.noreply.github.com>
2026-04-14 09:08:07 -0500

    Commit init

---
+++ Quiz-2-romero-castro-garcia/src/main/java/desarrolloempresarial/com/quiz2romerocastrogarcia/Quiz2RomeroCastroGarciaApplication.java
@@ -0,0 +1,13 @@
+package desarrolloempresarial.com.quiz2romerocastrogarcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz2RomeroCastroGarciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Quiz2RomeroCastroGarciaApplication.class, args);
+    }
+
+}
