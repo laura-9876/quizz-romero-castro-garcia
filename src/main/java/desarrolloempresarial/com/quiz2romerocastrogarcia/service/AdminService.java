489986721b8b557ef095e23ce04f8ec829e1083d
Lauren <164786167+Laurennkr@users.noreply.github.com>
2026-04-14 22:38:53 -0500

    add: seguridad de chef completada

--- src/main/java/desarrolloempresarial/com/quiz2romerocastrogarcia/service/AdminService.java
+++ src/main/java/desarrolloempresarial/com/quiz2romerocastrogarcia/service/AdminService.java
@@ -1,5 +1,32 @@
 package desarrolloempresarial.com.quiz2romerocastrogarcia.service;
 
-public class AdminService {
+
+import desarrolloempresarial.com.quiz2romerocastrogarcia.controller.dto.ChefRequestDto;
+import desarrolloempresarial.com.quiz2romerocastrogarcia.controller.dto.ChefResponseDto;
+import desarrolloempresarial.com.quiz2romerocastrogarcia.controller.dto.KitchenRequestDto;
+import desarrolloempresarial.com.quiz2romerocastrogarcia.controller.dto.KitchenResponseDto;
+
+import java.util.List;
+
+public interface AdminService {
+
+    ChefResponseDto createChef(ChefRequestDto request);
+
+    List<ChefResponseDto> getAllChefs();
+
+    ChefResponseDto getChefById(Long id);
+
+    ChefResponseDto updateChef(Long id, ChefRequestDto request);
+
+    void deleteChef(Long id);
+
+    KitchenResponseDto createKitchen(KitchenRequestDto request);
+
+    List<KitchenResponseDto> getAllKitchens();
+
+    KitchenResponseDto getKitchenById(Long id);
+
+    KitchenResponseDto updateKitchen(Long id, KitchenRequestDto request);
+
+    void deleteKitchen(Long id);
 }
-//ahsta que deje de salirte archivo pa actualziar
